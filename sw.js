e9fedd61fea2888184e896fb8de1dce2d6bb5676
AtsushiSora <closet.full.of.love.a@gmail.com>
2026-08-16 18:44:57 +0900

    Polish Juggle arcade button feedback

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
-const CACHE_NAME = "ichigeki-web-v79";
+const CACHE_NAME = "ichigeki-web-v80";
 const CORE_ASSETS = [
   "index.html",
   "juggle-simple.html",
   "tokyo-ghoul-999.html",
   "two-choice-select.html",
   "style.css",
-  "style.css?v=79",
+  "style.css?v=80",
   "main.js",
-  "main.js?v=79",
+  "main.js?v=80",
   "assets/juggle/start-button-v3.png",
   "assets/juggle/result-panel-v2.png",
   "assets/juggle/marquee-frame-v4.png",
