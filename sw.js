d8ad4e1847fcb8aed512c9157de906a4b88c9991
AtsushiSora <closet.full.of.love.a@gmail.com>
2026-08-16 17:46:49 +0900

    Slim Juggle arcade tabs

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
-const CACHE_NAME = "ichigeki-web-v76";
+const CACHE_NAME = "ichigeki-web-v77";
 const CORE_ASSETS = [
   "index.html",
   "juggle-simple.html",
   "tokyo-ghoul-999.html",
   "two-choice-select.html",
   "style.css",
-  "style.css?v=76",
+  "style.css?v=77",
   "main.js",
-  "main.js?v=76",
+  "main.js?v=77",
   "assets/juggle/start-frame.png",
   "assets/juggle/start-frame-v2.png",
   "assets/juggle/start-button-v3.png",
