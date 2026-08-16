6fd7f37fea6ed743144f90b9d9f0a1608c0491ea
AtsushiSora <closet.full.of.love.a@gmail.com>
2026-08-16 18:17:45 +0900

    Tighten Juggle arcade mobile layout

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
-const CACHE_NAME = "ichigeki-web-v78";
+const CACHE_NAME = "ichigeki-web-v79";
 const CORE_ASSETS = [
   "index.html",
   "juggle-simple.html",
   "tokyo-ghoul-999.html",
   "two-choice-select.html",
   "style.css",
-  "style.css?v=78",
+  "style.css?v=79",
   "main.js",
-  "main.js?v=78",
+  "main.js?v=79",
   "assets/juggle/start-button-v3.png",
   "assets/juggle/result-panel-v2.png",
   "assets/juggle/marquee-frame-v4.png",
