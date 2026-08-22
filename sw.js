37451a8540ce4a15017bd97701476d24d5c17bce
AtsushiSora <closet.full.of.love.a@gmail.com>
2026-08-22 16:55:50 +0900

    Make TOP page pixel arcade style

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
-const CACHE_NAME = "ichigeki-web-v86";
+const CACHE_NAME = "ichigeki-web-v87";
 const CORE_ASSETS = [
   "index.html",
   "juggle-simple.html",
   "tokyo-ghoul-999.html",
   "two-choice-select.html",
   "style.css",
-  "style.css?v=86",
+  "style.css?v=87",
   "main.js",
-  "main.js?v=86",
+  "main.js?v=87",
   "assets/juggle/start-button-v3.png",
   "assets/juggle/result-panel-v2.png",
   "assets/juggle/marquee-frame-v4.png",
