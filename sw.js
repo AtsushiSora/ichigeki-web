415bfb1fa082dfde725aa63a0ed9ad680c021cc9
AtsushiSora <closet.full.of.love.a@gmail.com>
2026-08-22 15:40:05 +0900

    Add space above Juggle start button

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
-const CACHE_NAME = "ichigeki-web-v83";
+const CACHE_NAME = "ichigeki-web-v84";
 const CORE_ASSETS = [
   "index.html",
   "juggle-simple.html",
   "tokyo-ghoul-999.html",
   "two-choice-select.html",
   "style.css",
-  "style.css?v=83",
+  "style.css?v=84",
   "main.js",
-  "main.js?v=83",
+  "main.js?v=84",
   "assets/juggle/start-button-v3.png",
   "assets/juggle/result-panel-v2.png",
   "assets/juggle/marquee-frame-v4.png",
