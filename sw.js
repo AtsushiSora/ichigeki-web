68deccab7ae7ed2d2a1f10573f64ac7f84922f38
AtsushiSora <closet.full.of.love.a@gmail.com>
2026-08-22 14:50:39 +0900

    Balance Juggle arcade mobile spacing

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
-const CACHE_NAME = "ichigeki-web-v82";
+const CACHE_NAME = "ichigeki-web-v83";
 const CORE_ASSETS = [
   "index.html",
   "juggle-simple.html",
   "tokyo-ghoul-999.html",
   "two-choice-select.html",
   "style.css",
-  "style.css?v=82",
+  "style.css?v=83",
   "main.js",
-  "main.js?v=82",
+  "main.js?v=83",
   "assets/juggle/start-button-v3.png",
   "assets/juggle/result-panel-v2.png",
   "assets/juggle/marquee-frame-v4.png",
